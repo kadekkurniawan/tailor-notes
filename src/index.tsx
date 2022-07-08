c13b5f2ac6c7067864b4a0d7cd1eaef287db3042
Esa Kurniawan <esakurniawan866@gmail.com>
2022-07-08 15:07:10 +0800

    Added service worker

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 
 import "./index.css";
 import App from "./App";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 const root = ReactDOM.createRoot(
     document.getElementById("root") as HTMLElement
@@ -16,3 +17,8 @@ root.render(
         </BrowserRouter>
     </React.StrictMode>
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
